c9dfb2f8ee8a3ddd4158f1ccbbb3f32c180351aa
RustiktamQ <rramilperm@gmail.com>
2024-03-29 14:27:49 +0500

    w

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const getData = async() => {
     try{
         const response = await axios.get("https://books.toscrape.com/")
-        console.log(response.data); // HTML
+        console.log(response.data);
     }
     catch(e) {
         console.log(e);
