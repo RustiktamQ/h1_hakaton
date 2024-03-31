13ca1f2c6d8a715d6c17e3aa260beec35617e861
mel1x <75519364+mel1x@users.noreply.github.com>
2024-03-31 07:04:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const updater = require('./libs/updater.js');
 const eventRouter = require('./routers/index.js');
 const userRouter = require('./routers/user.js')
 
+const d = new Date();
+d.setTime(1711843200000);
 updater.startUpdaterPerDay();
 // updater.updater();
 
